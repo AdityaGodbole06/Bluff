9489e07c76978d15954bd2aab96641c4174f567a
Aditya <aditya.godbole6@gmail.com>
2025-08-03 23:27:58 +1000

    Fix Vite base path for production and revert HTML

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Bluff",
+  base: process.env.NODE_ENV === 'production' ? '/' : '/Bluff',
   server: {
     proxy: {
       '/api': {
